02a3d5c15277802d8529f424542ec3f83015dcbe
Dan <yabomonkey@gmail.com>
2022-01-01 23:37:17 -0800

    Removed many lines of code, used just 1 to replace
    Prior method could be used for larger files though so might be useful in future.

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -41,51 +41,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             private fun downloadXML(urlPath: String?): String {
-                val xmlResult = StringBuilder()
-
-                try {
-                    val url = URL(urlPath)
-                    val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-                    val response = connection.responseCode
-                    Log.d(TAG, "downloadXML: The response code was $response")
-
-//                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
-//
-//                    val inputBuffer = CharArray(500)
-//                    var charsRead = 0
-//                    while (charsRead >= 0) {
-//                        charsRead = reader.read(inputBuffer)
-//                        if (charsRead > 0) {
-//                            xmlResult.append(String(inputBuffer, 0, charsRead))
-//                        }
-//                    }
-//                    reader.close()
-
-                    connection.inputStream.buffered().reader().use {xmlResult.append(it.readText())}
-
-                    Log.d(TAG, "Received ${xmlResult.length} bytes")
-                    return xmlResult.toString()
-
-//                } catch (e: MalformedURLException) {
-//                    Log.e(TAG, "downloadURL: Invalid URL ${e.message}")
-//                } catch (e: IOException) {
-//                    Log.e(TAG, "downloadURL: IO Exception reading data ${e.message}")
-//                } catch (e: SecurityException) {
-//                    Log.e(TAG, "downloadURL: Security Exception. Need permissions? ${e.message}")
-//                } catch (e: Exception) {
-//                    Log.e(TAG, "Unknown Error: ${e.message}")
-//                }
-                } catch (e: Exception) {
-                    val errorMessage: String = when(e) {
-                        is MalformedURLException -> "downloadURL: Invalid URL ${e.message}"
-                        is IOException -> "downloadURL: IO Exception reading data ${e.message}"
-                        is SecurityException -> {e.printStackTrace()
-                            "downloadURL: Security Exception. Need permissions? ${e.message}"
-                        }
-                        else -> "Unknown Error: ${e.message}"
-                    }
-                }
-                return ""
+                return URL(urlPath).readText()
             }
 
         }
