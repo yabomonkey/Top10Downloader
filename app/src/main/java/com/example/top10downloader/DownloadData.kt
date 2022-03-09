c8f09dc17856affe33c4fc65c260428f85b4ffd0
Dan <yabomonkey@gmail.com>
2022-03-09 00:36:13 -0800

    added Error Catching to downloadXML

--- app/src/main/java/com/example/top10downloader/DownloadData.kt
+++ app/src/main/java/com/example/top10downloader/DownloadData.kt
@@ -10,46 +10,44 @@ private const val TAG = "DownloadData"
 
 class DownloadData(private val callBack: DownloaderCallBack) : AsyncTask<String, Void, String>() {
 
-        interface DownloaderCallBack {
-            fun onDataAvailable(data: List<FeedEntry>)
-        }
-
-        override fun onPostExecute(result: String) {
-            val parseApplications = ParseApplications()
-
-            if (result.isNotEmpty()) {
-                parseApplications.parse(result)
-            }
-
-            callBack.onDataAvailable(parseApplications.applications)
-        }
-
-        override fun doInBackground(vararg url: String): String {
-            Log.d(TAG, "doInBackground: starts with ${url[0]}")
-            val rssFeed = downloadXML(url[0])
-            if (rssFeed.isEmpty()) {
-                Log.e(TAG, "doInBackground: error downloading")
-            }
-
-            return rssFeed
-        }
-
-        private fun downloadXML(urlPath: String): String {
-            try {
-                return URL(urlPath).readText()
-            } catch (e: MalformedURLException) {
-                Log.d((TAG, "downloadXML: Invalid URL " + e.message)
-            } catch (e: IOException) {
-                Log.d((TAG, "downloadXML: IO Exception reading data " + e.message)
-            } catch (e: SecurityException){
-                Log.d((TAG, "downloadXML: Security exception: Needs Permission? " + e.message)
-//                e.printStackTrace()
-            }
-
-            return ""           // return an empty string if there was an exception
-        }
-
-
-
+    interface DownloaderCallBack {
+        fun onDataAvailable(data: List<FeedEntry>)
     }
+
+    override fun onPostExecute(result: String) {
+        val parseApplications = ParseApplications()
+
+        if (result.isNotEmpty()) {
+            parseApplications.parse(result)
+        }
+
+        callBack.onDataAvailable(parseApplications.applications)
+    }
+
+    override fun doInBackground(vararg url: String): String {
+        Log.d(TAG, "doInBackground: starts with ${url[0]}")
+        val rssFeed = downloadXML(url[0])
+        if (rssFeed.isEmpty()) {
+            Log.e(TAG, "doInBackground: error downloading")
+        }
+
+        return rssFeed
+    }
+
+    private fun downloadXML(urlPath: String): String {
+        try {
+            return URL(urlPath).readText()
+        } catch (e: MalformedURLException) {
+            Log.d(TAG, "downloadXML: Invalid URL " + e.message)
+        } catch (e: IOException) {
+            Log.d(TAG, "downloadXML: IO Exception reading data " + e.message)
+        } catch (e: SecurityException) {
+            Log.d(TAG, "downloadXML: Security exception: Needs Permission? " + e.message)
+//                e.printStackTrace()
+        }
+
+        return ""           // return an empty string if there was an exception
+    }
+
+
 }
