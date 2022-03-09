7e64e344bef074cc7b9c39b251ff31c75e5bf56a
Dan <yabomonkey@gmail.com>
2022-03-09 00:21:26 -0800

    Moved all non-GUI related funtions to ViewModel

--- app/src/main/java/com/example/top10downloader/FeedViewModel.kt
+++ app/src/main/java/com/example/top10downloader/FeedViewModel.kt
@@ -1,4 +1,52 @@
 package com.example.top10downloader
 
-class FeedViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+private const val TAG = "FeedViewModel"
+
+val EMPTY_FEED_LIST: List<FeedEntry> = Collections.emptyList()
+
+class FeedViewModel : ViewModel(), DownloadData.DownloaderCallBack {
+    private var downloadData: DownloadData? = null
+    private var feedCachedURL = "INVALIDATED"
+
+    private val feed = MutableLiveData<List<FeedEntry>>()
+    val feedEntries: LiveData<List<FeedEntry>>
+        get() = feed
+
+    init {
+        feed.postValue(EMPTY_FEED_LIST)
+    }
+
+    fun downloadUrl(feedUrl: String){
+        Log.d(TAG, "downloadURL: Called with url $feedUrl")
+        if(feedUrl != feedCachedURL) {
+            Log.d(TAG, "downloadUrl starting AsyncTask")
+            downloadData = DownloadData(this)
+            downloadData?.execute(feedUrl)
+            feedCachedURL = feedUrl
+            Log.d(TAG, "downloadUrl Done")
+        } else {
+            Log.d(TAG, "downloadURL - URL not changed")
+        }
+    }
+
+    fun invalidate() {
+        feedCachedURL = "INVALIDATE"
+    }
+
+    override fun onDataAvailable(data: List<FeedEntry>) {
+        Log.d(TAG, "onDataAvailable called")
+        feed.value = data
+        Log.d(TAG, "onDataAvailable ends")
+    }
+
+    override fun onCleared() {
+        Log.d(TAG, "onCleared: canceling pending downloads")
+        downloadData?.cancel(true)
+    }
 }
